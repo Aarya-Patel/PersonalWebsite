041c16a682eb017923e3dba2bf62c17beff05409
aaryapatel <aarya.patel@mail.utoronto.ca>
2020-06-23 11:32:55 -0400

    Updates

---
+++ precache-manifest.57c1309f639c49402d7fc1b44e54e796.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6eac4cbcdf68ea87dd07056a1f4ced",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "217cdf9df066a767a7c9",
+    "url": "/PersonalWebsite/static/css/main.2539a82f.chunk.css"
+  },
+  {
+    "revision": "b17c08dc84228749f47d",
+    "url": "/PersonalWebsite/static/js/2.208d799b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalWebsite/static/js/2.208d799b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217cdf9df066a767a7c9",
+    "url": "/PersonalWebsite/static/js/main.cad5dc21.chunk.js"
+  },
+  {
+    "revision": "05a2b04fe66c16cd332d",
+    "url": "/PersonalWebsite/static/js/runtime-main.b2dd1bbe.js"
+  },
+  {
+    "revision": "dbafb5f70cbf6e9d1a0eb8adafa3cd52",
+    "url": "/PersonalWebsite/static/media/AaryaPatel-Resume.dbafb5f7.pdf"
+  },
+  {
+    "revision": "f657e220c2bfd0378456d8d9d7ea5d9c",
+    "url": "/PersonalWebsite/static/media/GitHub-Mark1.f657e220.png"
+  },
+  {
+    "revision": "a6d8c31c1fe79c43756815662954d424",
+    "url": "/PersonalWebsite/static/media/baps.a6d8c31c.jpg"
+  },
+  {
+    "revision": "bbfa0bd3d258eb18d7aef1f95f6295a1",
+    "url": "/PersonalWebsite/static/media/gmail.bbfa0bd3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PersonalWebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f23295ce52758220629997a1cd79d2fa",
+    "url": "/PersonalWebsite/static/media/profile.f23295ce.jpg"
+  },
+  {
+    "revision": "f346eac17a25d633601fb36159eb6f6e",
+    "url": "/PersonalWebsite/static/media/svgbottom.f346eac1.svg"
+  },
+  {
+    "revision": "7bc77f229f02e2942ac81c7bc20af4b1",
+    "url": "/PersonalWebsite/static/media/svgleft.7bc77f22.svg"
+  },
+  {
+    "revision": "85f1769c60ef40ace860d4d35b300437",
+    "url": "/PersonalWebsite/static/media/svgright.85f1769c.svg"
+  },
+  {
+    "revision": "09951e5f088ae044b6f55ef1f808ed35",
+    "url": "/PersonalWebsite/static/media/toronto.09951e5f.jpg"
+  },
+  {
+    "revision": "d5a245c9c3be650e64f7debab462ef58",
+    "url": "/PersonalWebsite/static/media/uoft.d5a245c9.jpg"
+  },
+  {
+    "revision": "e1ab1228bc1301ed7cc415da033e6e2f",
+    "url": "/PersonalWebsite/static/media/wonder.e1ab1228.png"
+  },
+  {
+    "revision": "fd32b34b44bb87e518de8a1ed1601dce",
+    "url": "/PersonalWebsite/static/media/wonderland.fd32b34b.png"
+  }
+]);
